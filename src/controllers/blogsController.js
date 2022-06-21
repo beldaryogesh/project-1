6303d3751bfa72e86a4443ab789dc211fa8f99a6
bidyut10 <bidyut34268@gmail.com>
2022-06-21 15:57:02 +0530

    editOnSchema

--- src/controllers/blogsController.js
+++ src/controllers/blogsController.js
@@ -2,9 +2,10 @@ const BlogsModel = require("../models/blogsModel")
 
 const createBlogs = async function (req, res) {
     let blog = req.body
-
+    let authorId= req.body.authorId
+    if(!authorId) res.status(400).send({status:false,msg:"Author is not present"})
     let blogsCreated = await BlogsModel.create(blog)
-    res.send({ data: blogsCreated })
+    res.status(201).send({ data: blogsCreated })
 }
 
 
