bb698b38c13accdb14a76174825a2c8ab6a7a307
bidyut10 <bidyut34268@gmail.com>
2022-06-23 13:26:24 +0530

    new branch

--- src/middlewares/comonMW.js
+++ src/middlewares/comonMW.js
@@ -1,16 +1,16 @@
 const jwt = require("jsonwebtoken");
-const authenticate = function(req, res, next) {
-try{
+const authenticate = function (req, res, next) {
+  try {
     let token = req.headers["x-auth-token"];
 
-  if (!token) token = req.headers["x-auth-token"];
-  if (!token) return res.send({ status: false, msg: "token must be present" });
+    if (!token) token = req.headers["x-auth-token"];
+    if (!token) return res.send({ status: false, msg: "token must be present" });
 
-  let decodedToken = jwt.verify(token, "functionup-radon");
+    let decodedToken = jwt.verify(token, "functionup-radon");
 
-  if (!decodedToken)
-    return res.send({ status: false, msg: "token is invalid" });
-    req.decodedToken=decodedToken
+    if (!decodedToken)
+      return res.send({ status: false, msg: "token is invalid" });
+    req.decodedToken = decodedToken
     next()
   } catch (err) {
     console.log("This is the error :", err.message);
@@ -18,61 +18,57 @@ try{
   }
 }
 
-const authorisation=function(req,res,next){
-  try{
-      // console.log(validateToken)
-      token = req.headers["x-auth-token"]
-      console.log(token)
-      let blog_Id = req.params.blogId
-      let userId = decodedToken.userId
-      let data = req.query; 
-      if(blog_Id.authorId==userId){
-      if(blog_Id){
-      if (!mongoose.isValidObjectId(blog_Id)) return res.status(400).send({ status: false, msg: "Enter a Valid BlogId"})
-      let authorData=await blogModel.findOne({_id:blog_Id,authorId:userId})
-      if (!authorData)return res.send({ status: false, msg:"you are not authorized" })
+const authorisation = function (req, res, next) {
+  try {
+    token = req.headers["x-auth-token"]
+    console.log(token)
+    let blog_Id = req.params.blogId
+    let userId = decodedToken.userId
+    let data = req.query;
+    if (blog_Id.authorId == userId) {
+      if (blog_Id) {
+        if (!mongoose.isValidObjectId(blog_Id)) return res.status(400).send({ status: false, msg: "Enter a Valid BlogId" })
+        let authorData = await blogModel.findOne({ _id: blog_Id, authorId: userId })
+        if (!authorData) return res.send({ status: false, msg: "you are not authorized" })
       }
 
-      
-     
-      if(data.authorId){
-          if (!mongoose.isValidObjectId(data.authorId)) return res.status(400).send({ status: false, msg: "Enter a Valid authorId"})
-          if(data.authorId!=userId)return res.send({ status: false, msg:"you are not authorized" })  
+      if (data.authorId) {
+        if (!mongoose.isValidObjectId(data.authorId)) return res.status(400).send({ status: false, msg: "Enter a Valid authorId" })
+        if (data.authorId != userId) return res.send({ status: false, msg: "you are not authorized" })
       }
 
-      
-      if(data.category){
-          let authorData=await blogModel.find({category:data.category,authorId:userId})
-          if (!authorData.length)return res.send({ status: false, msg:"you are not authorized" })  
+
+      if (data.category) {
+        let authorData = await blogModel.find({ category: data.category, authorId: userId })
+        if (!authorData.length) return res.send({ status: false, msg: "you are not authorized" })
       }
 
-      if(data.subcategory){
-          let authorData=await blogModel.find({subcategory:data.subcategory,authorId:userId})
-          if (!authorData.length)return res.send({ status: false, msg:"you are not authorized" }) 
+      if (data.subcategory) {
+        let authorData = await blogModel.find({ subcategory: data.subcategory, authorId: userId })
+        if (!authorData.length) return res.send({ status: false, msg: "you are not authorized" })
       }
 
-      if(data.tags){
-          let authorData=await blogModel.find({tags:data.tags,authorId:userId})
-          if (!authorData.length)return res.send({ status: false, msg:"you are not authorized" })   
+      if (data.tags) {
+        let authorData = await blogModel.find({ tags: data.tags, authorId: userId })
+        if (!authorData.length) return res.send({ status: false, msg: "you are not authorized" })
       }
 
-      if(data.isPublished){
-          let authorData=await blogModel.find({isPublished:data.isPublished,authorId:userId})
-          if (!authorData.length)return res.send({ status: false, msg:"you are not authorized" })
+      if (data.isPublished) {
+        let authorData = await blogModel.find({ isPublished: data.isPublished, authorId: userId })
+        if (!authorData.length) return res.send({ status: false, msg: "you are not authorized" })
       }
 
       next()
-}}
+    }
+  }
 
   catch (error) {
 
-      res.status(500).send(error.message)
+    res.status(500).send(error.message)
 
   }
 }
 
+module.exports.authorisation = authorisation
 
-
-module.exports.authorisation=authorisation
-
-module.exports.authenticate=authenticate
+module.exports.authenticate = authenticate
